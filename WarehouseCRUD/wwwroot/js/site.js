d65300a220897c3ded500d6cfa32933028ab5ac2
PapaGrando <borislovesalo@gmail.com>
2022-04-12 05:44:29 +0300

    styles added

--- WarehouseCRUD/wwwroot/js/site.js
+++ WarehouseCRUD/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const img = document.getElementById("productImage")
+img.addEventListener("error", function (event) {
+    event.target.src = "~/images/DefaultProd.png"
+    event.onerror = null
+})
