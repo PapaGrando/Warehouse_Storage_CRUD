bd5b8befd7d34917fc068f14e993b8e80c7f921a
PapaGrando <borislovesalo@gmail.com>
2022-04-12 05:09:02 +0300

    prod db context

--- WarehouseCRUD/Startup.cs
+++ WarehouseCRUD/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WarehouseCRUD.Storage.DataContext;
 
 namespace WarehouseCRUD.Storage
 {
@@ -24,6 +26,10 @@ namespace WarehouseCRUD.Storage
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<StorageDbContext>(opt =>
+                opt.UseNpgsql(Configuration.GetConnectionString("PgConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
