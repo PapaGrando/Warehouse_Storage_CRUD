0b6bf8fdf7e32773d9a5a65e8b1bb8a019b1f8cf
PapaGrando <borislovesalo@gmail.com>
2022-04-12 05:08:39 +0300

    base prod operations

---
+++ WarehouseCRUD/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WarehouseCRUD.Storage.DataContext;
+using WarehouseCRUD.Storage.Models;
+
+namespace WarehouseCRUD.Storage.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WarehouseCRUD.Storage.DataContext.StorageDbContext _context;
+
+        public CreateModel(WarehouseCRUD.Storage.DataContext.StorageDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ WarehouseCRUD/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WarehouseCRUD.Storage.DataContext;
+using WarehouseCRUD.Storage.Models;
+
+namespace WarehouseCRUD.Storage.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WarehouseCRUD.Storage.DataContext.StorageDbContext _context;
+        private readonly IWebHostEnvironment _appEnvironment;
+
+        public IndexModel(WarehouseCRUD.Storage.DataContext.StorageDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; }
+        public string DefaultImage => Path.Combine(_appEnvironment.WebRootPath, "DefaultProd.png");
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
+        }
+    }
+}
