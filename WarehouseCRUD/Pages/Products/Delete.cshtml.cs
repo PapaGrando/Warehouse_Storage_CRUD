82bd931b1bbfdf6bce4e2fd4eebebb06a2d44955
PapaGrando <borislovesalo@gmail.com>
2022-04-12 05:39:52 +0300

    prod deleting

---
+++ WarehouseCRUD/Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WarehouseCRUD.Storage.DataContext;
+using WarehouseCRUD.Storage.Models;
+
+namespace WarehouseCRUD.Storage.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly WarehouseCRUD.Storage.DataContext.StorageDbContext _context;
+
+        public DeleteModel(WarehouseCRUD.Storage.DataContext.StorageDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product = await _context.Products.FindAsync(id);
+
+            if (Product != null)
+            {
+                _context.Products.Remove(Product);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
