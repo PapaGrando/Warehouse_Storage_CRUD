3481c9d728f0f609cb8ccccd0ddfa7ea37312f7d
PapaGrando <borislovesalo@gmail.com>
2022-04-12 05:46:53 +0300

    new models

--- WarehouseCRUD/Models/Storage/CellType.cs
+++ WarehouseCRUD/Models/Storage/CellType.cs
@@ -1,12 +1,38 @@
-﻿namespace WarehouseCRUD.Storage.Models.Storage
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseCRUD.Storage.Models.Storage
 {
     /// <summary>
     /// Тип ячейки, обозначающая, какой тип товаров можно хранить
-    /// Мелкий, Крупногабарит, Негабарит, Еда, Хрупкое
+    /// Мелкий, Ячейки мезонина, Крупногабаритный стеллаж, Напольные ячейки
     /// </summary>
     public class CellType
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(25)]
         public string Name { get; set; }
+
+        [Required]
+        [Range(0.0f, float.MaxValue)]
+        [Display(Name = "Вес кг")]
+        public float MaxWeight { get; set; }
+
+        [Required]
+        [Range(0.0, int.MaxValue)]
+        [Display(Name = "Длина см")]
+        public int Length { get; set; }
+
+        [Required]
+        [Range(0.0, int.MaxValue)]
+        [Display(Name = "Ширина см")]
+        public int Width { get; set; }
+
+        [Required]
+        [Range(0.0, int.MaxValue)]
+        [Display(Name = "Высота см")]
+        public int Height { get; set; }
     }
 }
--- WarehouseCRUD/Models/Storage/StorageItems.cs
+++ WarehouseCRUD/Models/Storage/StorageItems.cs
@@ -7,6 +7,22 @@ namespace WarehouseCRUD.Storage.Models.Storage
         [Key]
         public int Id { get; set; }
         public Product InfoId { get; set; }
-        public Cell CellId { get; set; }
+        public Cell Cell { get; set; }
+        public StorageState State { get; set; }
+    }
+
+    public enum StorageState
+    {
+        [Display(Name = "Не расположен")]
+        None,
+
+        [Display(Name = "Ожидает приема")]
+        Arrival,
+
+        [Display(Name = "В хранилище")]
+        OnCell,
+
+        [Display(Name = "В отгрузке")]
+        OnShipment
     }
 }
